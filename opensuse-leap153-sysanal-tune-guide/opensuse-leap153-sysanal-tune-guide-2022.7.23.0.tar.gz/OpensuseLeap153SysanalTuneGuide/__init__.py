c31f09a7aff8d5be3d6b5654cb3197b7a1f4309d
Tom Forbes <tom@tomforb.es>
2022-07-23 14:27:52 +0000

    {"name":"opensuse-leap153-sysanal-tune-guide","version":"2022.7.23.0","file":"opensuse-leap153-sysanal-tune-guide-2022.7.23.0.tar.gz","path":"opensuse-leap153-sysanal-tune-guide/opensuse-leap153-sysanal-tune-guide-2022.7.23.0.tar.gz"}

---
+++ opensuse-leap153-sysanal-tune-guide/opensuse-leap153-sysanal-tune-guide-2022.7.23.0.tar.gz/OpensuseLeap153SysanalTuneGuide/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""opensuse-leap153-sysanal-tune-guide
+https://github.com/apachecn/opensuse-leap153-sysanal-tune-guide"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.23.0"
